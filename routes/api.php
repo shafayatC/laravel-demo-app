51396ae6ab4745bb5d6231184758adf73fc22ba8
shafayat18 <shafayatplay@gmail.com>
2025-03-04 14:26:57 +0600

    added midleware

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,13 @@ Route::get('/test', function () {
     return response()->json(['message' => 'Hello World!']);
 });
 
-Route::get('students', [StudentController::class, 'list']);
-Route::post('add-student', [StudentController::class, 'addStudent']);
-Route::put('update-student', [StudentController::class, 'updateStudent']);
-Route::delete('delete-student/{id}', [StudentController::class, 'deleteStudent']);
-Route::get('search-student/{name}', [StudentController::class, 'searchStudent']);
 Route::post('signup',  [UserAuthController::class, 'signup']);
 Route::post('login',  [UserAuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::get('students', [StudentController::class, 'list']);
+    Route::post('add-student', [StudentController::class, 'addStudent']);
+    Route::put('update-student', [StudentController::class, 'updateStudent']);
+    Route::delete('delete-student/{id}', [StudentController::class, 'deleteStudent']);
+    Route::get('search-student/{name}', [StudentController::class, 'searchStudent']);
+});
